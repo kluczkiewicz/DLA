8dc7478b826a1fe1af16adaf60a4bef69c0e4d66
Kamil <kamil@MacBook-Air-Kamil.local>
2023-06-18 20:13:11 +0200

    init commit

---
+++ dla.py
@@ -0,0 +1,74 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+def init_grid(box_size = 101):
+    # initialize grid with zeros
+    global N 
+    N = box_size
+    global grid
+    grid = np.zeros((N, N), dtype=int)
+    # place seed in the center
+    grid[N//2][N//2] = 1
+    
+
+def is_neighbour(i, j):
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            if x == y == 0:
+                continue
+            elif i+x < 0 or i+x >= N or j+y < 0 or j+y >= N:
+                continue
+            elif grid[i+x][j+y] == 1:
+                return True
+    return False
+
+def random_walker():
+    # starting the walker at a random position at the edge
+    edge = random.choice(['up', 'down', 'left', 'right'])
+    if edge == 'up':
+        i, j = 0, random.randint(0, N-1)
+    elif edge == 'down':
+        i, j = N-1, random.randint(0, N-1)
+    elif edge == 'left':
+        i, j = random.randint(0, N-1), 0
+    else:  # edge == 'right'
+        i, j = random.randint(0, N-1), N-1
+
+    # keep walking until the walker sticks or goes off the edge
+    while True:
+        direction = random.choice(['up', 'down', 'left', 'right'])
+        if direction == 'up':
+            i -= 1
+        elif direction == 'down':
+            i += 1
+        elif direction == 'left':
+            j -= 1
+        else:  # direction == 'right'
+            j += 1
+
+        # if the walker has left the grid, start a new walker
+        if i < 0 or i >= N or j < 0 or j >= N:
+            return
+
+        # if the walker is neighbouring a part of the aggregate, add it to the aggregate and start a new walker
+        if is_neighbour(i, j):
+            grid[i][j] = 1
+            return
+
+def visualize(n):
+    plt.figure(figsize=(6, 6))
+    plt.imshow(grid, cmap='gray')
+    plt.title('Step ' + str(n))
+    plt.show()
+
+def generate_dla(k, m):
+    grids = []
+    for n in range(k):  # number of walkers
+        random_walker()
+        if n % m == 0:  # change this to control frequency of visualizations
+            #visualize(n)
+            grids.append(np.copy(grid))
+
+    return np.array(grids)
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from dla import generate_dla, init_grid
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import streamlit.components.v1 as components
+
+st.title('Diffusion-limited aggregation')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    box_size = st.number_input('Box size:', value = 0)
+with col2:
+    n_iterations = st.number_input('Number of iterations:', value = 0)
+with col3:
+    freq = st.number_input('Save frame every nth iterations:', value = 0)
+
+if st.button('Generate new pattern'):
+    with st.spinner('Generating...'):
+        init_grid(box_size)
+        images = generate_dla(n_iterations, freq)
+        fig = plt.figure()
+        im = plt.imshow(images[0], animated=True)
+        def updatefig(frame):
+            im.set_array(images[frame])
+            return [im]
+        ani = animation.FuncAnimation(fig, updatefig, frames = images.shape[0], blit=True)
+        plot = ani.to_jshtml()
+
+    components.html(plot, height=1000)
